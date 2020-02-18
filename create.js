9f75d105b03fceeda9296d7403f0f8729d691af6
William Redmond <bill@wgredmond.com>
2020-02-17 23:08:47 -0500

    Remove logging

--- create.js
+++ create.js
@@ -4,8 +4,6 @@ import { success, failure } from "./libs/response-lib";
 
 export async function main(event, context) {
   const data = JSON.parse(event.body);
-  console.log("cognitoIdentityId:");
-  console.log(event.requestContext.identity.cognitoIdentityId);
   const params = {
     TableName: process.env.tableName,
     Item: {
@@ -21,8 +19,6 @@ export async function main(event, context) {
     await dynamoDbLib.call("put", params);
     return success(params.Item);
   } catch (e) {
-    console.log("Error in create.");
-    console.log(e);
     return failure({ status: false });
   }
 }
